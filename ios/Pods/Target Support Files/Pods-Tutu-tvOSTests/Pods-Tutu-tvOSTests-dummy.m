0f5b4addc243c9109a4a551f80b8cdabbdf50179
qingyiyin <your_email@example.com>
2018-08-03 00:37:57 -0400

    Created Tutu honour project

---
+++ ios/Pods/Target Support Files/Pods-Tutu-tvOSTests/Pods-Tutu-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Tutu_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_Tutu_tvOSTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-TutuTests/Pods-TutuTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TutuTests : NSObject
+@end
+@implementation PodsDummy_Pods_TutuTests
+@end
