2587225fd0842b06623ee6caa20f7cbc642f8e51
qingyiyin <your_email@example.com>
2018-09-10 19:32:32 -0400

    added new commits

--- App.js
+++ App.js
@@ -1,15 +1,40 @@
 import React, {Component} from 'react';
-import {createStackNavigator, createBottomTabNavigator} from 'react-navigation';
+import {View, Text} from 'react-native';
+import {createStackNavigator, TabNavigator, createBottomTabNavigator} from 'react-navigation';
 import {LoginPage} from "./component/LoginPage";
-import {HomePage, HomeStack} from "./component/HomePage";
+import {HomePage} from "./component/HomePage";
 import {SignUpPage} from "./component/SignUpPage";
 import {Icon} from 'react-native-elements'
-import {PostStack} from "./component/PostPage/PostPage";
-import {AddNewPostStack} from "./component/PostPage/AddNewPostPage";
 
-
+export const HomeStack = createStackNavigator({
+    Home: {
+        screen: HomePage,
+        navigationOptions: {
+            title: 'Discover'
+        }
+    }
+});
 
 export const Tabs = createBottomTabNavigator({
+    Create: {
+        screen: HomePage,
+        navigationOptions: {
+            tabBarLabel: 'Your post',
+            tabBarIcon: ({tintColor}) => (
+                <Icon name="book-plus" size={22} color={tintColor} type='material-community'/>
+
+            )
+        }
+    },
+    WishList: {
+        screen: HomePage,
+        navigationOptions: {
+            tabBarLabel: 'Wish List',
+            tabBarIcon: ({tintColor}) => (
+                <Icon name="heart-outline" size={22} color={tintColor} type='material-community'/>
+            )
+        }
+    },
     Discover: {
         screen: HomeStack,
         navigationOptions: {
@@ -29,25 +54,6 @@ export const Tabs = createBottomTabNavigator({
             )
         }
     },
-    Post: {
-        screen: PostStack,
-        navigationOptions: {
-            tabBarLabel: 'Your post',
-            tabBarIcon: ({tintColor}) => (
-                <Icon name="book-plus" size={22} color={tintColor} type='material-community'/>
-
-            )
-        }
-    },
-    WishList: {
-        screen: HomePage,
-        navigationOptions: {
-            tabBarLabel: 'Wish List',
-            tabBarIcon: ({tintColor}) => (
-                <Icon name="heart-outline" size={22} color={tintColor} type='material-community'/>
-            )
-        }
-    },
     Profile: {
         screen: HomePage,
         navigationOptions: {
@@ -58,7 +64,7 @@ export const Tabs = createBottomTabNavigator({
         }
     }
 }, {
-    initialRouteName: 'Post',
+    initialRouteName: 'Discover',
     lazyLoad: true,
     animationEnabled: true,
     tabBarOptions: {
@@ -70,26 +76,9 @@ export const Tabs = createBottomTabNavigator({
     },
 });
 
-export const Root = createStackNavigator({
-    Login:{
-        screen: LoginPage
-    },
-    SignUp: {
-        screen: SignUpPage
-    },
-    Tabs: {
-        screen: Tabs,
-    },
-    AddNewPost: {
-        screen: AddNewPostStack,
-    }
-}, {
-    mode: 'modal',
-    headerMode: 'none'
-});
 
 export default class App extends Component {
-    render() {
-        return <Root/>;
+    render () {
+        return <Tabs/>;
     }
 }
--- component/HomePage.js
+++ component/HomePage.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import {View, Text, Image} from 'react-native';
 import {Icon, Button, SearchBar} from 'react-native-elements';
-import {createStackNavigator} from "react-navigation";
 
 export class HomePage extends Component {
 
@@ -29,11 +28,3 @@ export class HomePage extends Component {
     }
 }
 
-export const HomeStack = createStackNavigator({
-    Home: {
-        screen: HomePage,
-        navigationOptions: {
-            title: 'Discover'
-        }
-    }
-});
